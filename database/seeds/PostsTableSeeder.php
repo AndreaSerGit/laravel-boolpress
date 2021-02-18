5339eccdb96f1401a349fac9e83e4b335ea46798
Andrea Serboli <andrea.serboli12@gmail.com>
2021-02-18 16:06:11 +0100

    inizio progetto

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 15; $i++) { 
+            
+            $newPost = new Post();
+
+            $newPost->titolo = $faker->sentence(3);
+            $newPost->testo = $faker->text(2000);
+            $newPost->autore = $faker->name;
+            $newPost->foto = $faker->imageUrl(640, 480, 'nature');
+            $newPost->data_pubblicazione = $faker->dateTime
+            ();
+            $newPost->save();
+        }
+    }
+}
